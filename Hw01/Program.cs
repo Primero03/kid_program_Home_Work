de8fda331e3e652608201d95e4168063a7712e87
Primero <denis01102000pio03@gmail.com>
2022-11-27 13:30:56 +0300

    Программа,которая принимает число,и отвечает,является ли число четным или нет

---
+++ Hw01/Program.cs
@@ -0,0 +1,44 @@
+﻿// Домашка01_01
+//Console.WriteLine("Введите первое число:");
+//int number1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число:");
+//int number2 = int.Parse(Console.ReadLine());
+
+//int max = number1;
+//if (number2 > max) max = number2;
+//Console.Write("Максимальное число:");
+//Console.WriteLine(max);
+
+//int min = number1;
+//if (number2 < min) min = number2;
+//Console.Write("Минимальное число:");
+//Console.WriteLine(min);
+
+//Домашка01.02
+
+//Console.WriteLine("Введите первое число:");
+//int number1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число:");
+//int number2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите третье число:");
+//int number3 = int.Parse(Console.ReadLine());
+
+
+//int max = number1;
+//if (number2 > max) max = number2;
+//if (number3 > max) max = number3;
+//Console.Write("Максимальное число:");
+//Console.WriteLine(max);
+
+//Домашка01.03
+
+//Console.WriteLine("Ваше число");
+//int a = int.Parse(Console.ReadLine());
+
+//if (a % 2 ==0)
+//{
+//    Console.WriteLine("Число четное");
+
+//}
+//else
+//Console.WriteLine("Число не является четным");
