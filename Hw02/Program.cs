d4de18d1f01280a62cf13dedd1cee2f36a5f4910
Primero <denis01102000pio03@gmail.com>
2022-12-04 16:08:29 +0300

    homework02

--- Hw02/Program.cs
+++ Hw02/Program.cs
@@ -1,76 +1,79 @@
 ﻿/// Домашка02.01-Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+/*
+ string num = "Введите трёхзначное число ";
+ int range = 0;
 
- var phrase = "Введите трёхзначное число ";
- int someNumber = 0;
-
- while(someNumber>1000 || someNumber<100)
+ while(range>1000 || range<100)
  {
-     Console.WriteLine(phrase);
-     someNumber = int.Parse(Console.ReadLine());
-     phrase = "Введенное число вне диапазона! Введите трехзначное число ";
+     Console.WriteLine(num);
+     range = int.Parse(Console.ReadLine());
+     num = "Введенное число вне диапазона! Введите трехзначное число ";
  }
- int secondNumberOfSomeNumber = someNumber/10%10;
- Console.WriteLine(secondNumberOfSomeNumber);
+ int twoNumber = range/10%10;
+ Console.WriteLine(twoNumber);
+ */
 //Домашка02.02-Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-//Console.Write("Введите число ");
-// var someNumber2 = int.Parse(Console.ReadLine());
+/*
+ Console.Write("Введите число ");
+ int range = int.Parse(Console.ReadLine());
 
-// var thirdNumberOfSomeNumber2 = someNumber2;
-// if(thirdNumberOfSomeNumber2 < 0)
-// {
-//     thirdNumberOfSomeNumber2 = thirdNumberOfSomeNumber2 * -1;
-// }
+ int num = range;
+ if(num < 0)
+ {
+     num = num * -1;
+ }
 
-// while(thirdNumberOfSomeNumber2>999)
-// {
-//     thirdNumberOfSomeNumber2 = thirdNumberOfSomeNumber2 / 10;
-// }
-// if(thirdNumberOfSomeNumber2 < 100)
-// {
-//     Console.WriteLine("Третьей цифры нет");
-// }
-// else
-// {
-//     Console.WriteLine(thirdNumberOfSomeNumber2 % 10)
-
+ while(num>999)
+ {
+     num = num / 10;
+ }
+ if(num < 100)
+ {
+     Console.WriteLine("Третьей цифры нет");
+ }
+ else
+ {
+     Console.WriteLine(num % 10);
+ }   
+*/
 ///Домашка02.03-Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-//int a = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 
-//if (a == 1){
-//    Console.WriteLine("Понедельник");
-//    Console.Write("Будний день");
-//}
-//else if (a == 2)
-//{
-//    Console.WriteLine("Вторник");
-//    Console.Write("Будний день");
-//}
-//else if (a == 3)
-//{
-//    Console.WriteLine("Среда");
-//    Console.Write("Будний день");
-//}
-//else if (a == 4)
-//{
-//    Console.WriteLine("Четверг");
-//    Console.Write("Будний день");
-//}
-//else if (a == 5)
-//{
-//    Console.WriteLine("Пятница");
-//    Console.Write("Будний день");
-//}
-//else if (a == 6)
-//{
-//    Console.WriteLine("Суббота");
-//    Console.Write("Выходной");
-//}
-//else if (a == 7)
-//{
-//    Console.WriteLine("Воскресенье");
-//    Console.Write("Выходной");
-//}
-//else 
-//{
-//    Console.WriteLine("EROR");
-//}
+if (a == 1){
+    Console.WriteLine("Понедельник");
+    Console.Write("Будний день");
+}
+else if (a == 2)
+{
+    Console.WriteLine("Вторник");
+    Console.Write("Будний день");
+}
+else if (a == 3)
+{
+    Console.WriteLine("Среда");
+    Console.Write("Будний день");
+}
+else if (a == 4)
+{
+    Console.WriteLine("Четверг");
+    Console.Write("Будний день");
+}
+else if (a == 5)
+{
+    Console.WriteLine("Пятница");
+    Console.Write("Будний день");
+}
+else if (a == 6)
+{
+    Console.WriteLine("Суббота");
+    Console.Write("Выходной");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Воскресенье");
+    Console.Write("Выходной");
+}
+else 
+{
+    Console.WriteLine("EROR");
+}
