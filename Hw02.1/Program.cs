46451a1c1b00e4b0e510ad048f677daa66400ba1
Primero <denis01102000pio03@gmail.com>
2022-12-25 12:30:25 +0300

    02.02

--- Hw02.1/Program.cs
+++ Hw02.1/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
 Console.WriteLine("Введите трехзначное число");
 bool isParsed = int.TryParse(Console.ReadLine(), out int num);
 
@@ -13,3 +14,27 @@ while (!isParsed || num < 100 || num > 1000)
     int numTwo = num / 10 % 10;
 
 Console.WriteLine(numTwo);
+*/
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+Console.Write("Ваше число:");
+bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+
+while (!isParsed || num < 0)
+{
+    num = num * -1;
+}
+while (num > 999)
+{
+    num = num / 10;
+}
+if(num < 100)
+{
+    Console.WriteLine($"Третьей цифры в {num} нет");
+    Console.WriteLine("Попробуй еще раз");
+    num = int.Parse(Console.ReadLine());
+}
+int numTri = num % 10;
+Console.WriteLine(numTri);
+*/
