cd6578005a1f2a08dfd40a942043a72fb674715c
Primero <denis01102000pio03@gmail.com>
2022-12-25 14:50:12 +0300

    03.02

--- Hw03/Program.cs
+++ Hw03/Program.cs
@@ -4,7 +4,7 @@
 //Домашка03.02-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве. 
 //Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
 /*
-double WC ()
+double P3 ()
 {
     Console.WriteLine("Ваше число");
     bool isParsed = double.TryParse(Console.ReadLine(), out double num);
@@ -19,12 +19,12 @@ double WC ()
     }
 }
 
-double a1 = WC();
-double a2 = WC();
-double a3 = WC();
-double b1 = WC();
-double b2 = WC();
-double b3 = WC();
+double a1 = P3();
+double a2 = P3();
+double a3 = P3();
+double b1 = P3();
+double b2 = P3();
+double b3 = P3();
 
 double ab = Math.Sqrt((b1-a1)*(b1-a1) + (b2-a2)*(b2-a2) + (b3-a3)*(b3-a3));
 Console.WriteLine(ab);
@@ -32,3 +32,27 @@ Console.WriteLine(ab);
 
 //Домашка03.03-Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
 //Задание должно быть выполнено в методе и метод должен вернуть массив чисел.
+
+int P3 ()
+{
+    Console.WriteLine("Ваше число");
+    bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+    if (isParsed)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введенные символы,не являются верными в данном контексте");
+        return num = int.Parse(Console.ReadLine());
+    }
+}
+
+int num = P3();
+int i = 0;
+while(i <= num)
+{
+ int num3 = (int)(Math.Pow(i, 3));
+ Console.Write(num3 + " ");
+ i++;
+}
