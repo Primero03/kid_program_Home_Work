d1e96839491034b9935128fd8a53325939bdde62
Primero <denis01102000pio03@gmail.com>
2022-12-25 12:13:28 +0300

    02.01

---
+++ Hw02.1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+
+while (!isParsed || num < 100 || num > 1000)
+{
+    Console.WriteLine("Данное число не подходит под нужный диапозон");
+    Console.WriteLine("Повторите попытку");
+    num = int.Parse(Console.ReadLine());  
+}
+
+    Console.WriteLine(num);
+    int numTwo = num / 10 % 10;
+
+Console.WriteLine(numTwo);
