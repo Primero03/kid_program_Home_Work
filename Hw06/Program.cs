029de0a001c613c991ff2c1aec1f4f6791e2c2e3
Primero <denis01102000pio03@gmail.com>
2023-01-21 13:45:35 +0300

    02

--- Hw06/Program.cs
+++ Hw06/Program.cs
@@ -1,4 +1,21 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿//Домашка 06.01
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+double P3()
+{
+    Console.WriteLine("Ваше число");
+    bool isParsed = double.TryParse(Console.ReadLine(), out double num);
+    if (isParsed)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Введенные символы,не являются верными в данном контексте");
+        return num = double.Parse(Console.ReadLine());
+    }
+}
+
 
 // Console.WriteLine("Какой длины будет массив?");
 // int arrLentgth = int.Parse(Console.ReadLine());
@@ -21,6 +38,22 @@
 // System.Console.WriteLine(count);
 // System.Console.WriteLine('[' + string.Join("• ", Array) + ']');
 
-//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//Домашка06.02
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 //значения b1, k1, b2 и k2 задаются пользователем.
+
+
+double k1 =P3();
+double b1 =P3();
+double k2 =P3();
+double b2 =P3();
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
 
