17926d2797f0dfcd3e7306ad9332ea3fad2d1410
Primero <denis01102000pio03@gmail.com>
2023-01-21 13:18:45 +0300

    01

---
+++ Hw06/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Console.WriteLine("Какой длины будет массив?");
+// int arrLentgth = int.Parse(Console.ReadLine());
+// int[] Array = new int[arrLentgth];
+// int i = 0;
+// while (i < arrLentgth)
+// {
+//     Console.Write($"Введите {i + 1} число: ");
+//     Array[i] = int.Parse(Console.ReadLine());
+//     i++;
+// }
+// int count = 0;
+// for(i = 0; i < Array.Length; i++)
+// {
+//     if(Array[i]>0)
+//     {
+//         count+=1;
+//     }
+// }
+// System.Console.WriteLine(count);
+// System.Console.WriteLine('[' + string.Join("• ", Array) + ']');
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
