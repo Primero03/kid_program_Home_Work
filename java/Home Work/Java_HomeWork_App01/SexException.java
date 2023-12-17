d469f2881ef5a90145eb4dfdb670ed4170ddb177
Primero03 <denis01102000pio03@gmail.comm>
2023-12-17 14:32:35 +0300

    Приложение на запись внесеных данных о пользователе в файл

---
+++ java/Home Work/Java_HomeWork_App01/SexException.java
@@ -0,0 +1,12 @@
+package Java_HomeWork_App01;
+
+public class SexException extends Exception {
+    public SexException() {
+    }
+
+    public void sexException(String i) {
+        System.out.println("Exception: SexNotCorrectData");
+        System.out.printf("It`s not correct data: %s", i);
+        System.out.println();
+    }
+}
